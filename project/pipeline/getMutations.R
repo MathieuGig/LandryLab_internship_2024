984b6b7fffe0a3cb4b4b1b7b402ae3f790d5f26e
Mathieu Giguère <102560406+MathieuGig@users.noreply.github.com>
2024-07-29 16:34:50 -0400

    start of vcf exploration

---
+++ project/pipeline/getMutations.R
@@ -0,0 +1,25 @@
+#if (!require("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+
+#BiocManager::install("VariantAnnotation")
+
+library(VariantAnnotation)
+
+getwd()
+
+#fl <- system.file("extdata", "C:/Users/User/Documents/GitHub/stage-ete2024/project/pipeline/all.vcf", package="VariantAnnotation")
+
+fl <- "C:/Users/User/Documents/GitHub/stage-ete2024/project/pipeline/all.vcf"
+
+vcf <- readVcf(fl, "calls")
+
+samples(header(vcf))
+
+reference <- ref(vcf)
+mutations <- alt(vcf)
+
+#ref_genome <- "C:/Users/User/Documents/GitHub/stage-ete2024/project/synth_genome/my_synthetic_genome.txt"
+#library(BSgenome.Hsapiens.UCSC.hg19)
+#library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+#txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+#coding <- predictCoding(vcf, txdb, seqSource=ref_genome)
